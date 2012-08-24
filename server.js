fa9530e1506ed54613ec726683c598081705a750
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-08-24 12:17:42 +0100

    Respond to X-Explore-Upstream header to proxy multiple hosts

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ var Transform = proxy.Transform;
 var PORT = process.env.REVIEWOMATIC_EXPLORE_PORT || process.env.PORT || 8096;
 var REVIEWOMATIC_AUTH = process.env.REVIEWOMATIC_AUTH;
 var REVIEWOMATIC_HOST = process.env.REVIEWOMATIC_HOST || 'reviewomatic.production.alphagov.co.uk';
-var UPSTREAM_HOST = process.env.UPSTREAM_HOST || 'www.direct.gov.uk';
 
 if (!REVIEWOMATIC_AUTH) {
 	throw "You must set the REVIEWOMATIC_AUTH environment variable to auth credentials in the form 'username:password'!";
 }
 
 var reviewomaticProxy = new Proxy(REVIEWOMATIC_HOST, false, 'http', REVIEWOMATIC_AUTH);
-var upstreamProxy = new Proxy(UPSTREAM_HOST, true);
 
 http.createServer(function (req, res) {
+	var upstreamHost;
+	var upstreamProxy;
 	var ip = req.connection.remoteAddress;
 
 	util.log(ip + ": " + req.method + " " + req.url);
@@ -29,6 +29,8 @@ http.createServer(function (req, res) {
 	if (req.url.match(/^\/__/)) {
 		reviewomaticProxy.request(req, res);
 	} else {
+		upstreamHost = req.headers['x-explore-upstream'] || 'www.direct.gov.uk';
+		upstreamProxy = new Proxy(upstreamHost, true);
 		upstreamProxy.request(req, res);
 	}
 
