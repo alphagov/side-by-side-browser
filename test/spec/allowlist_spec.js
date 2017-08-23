4e503da39432647e86c7ac6d4bef8a7ee3c66cf6
Paul Bowsher <paul.bowsher@gmail.com>
2017-08-23 11:51:07 +0100

    Fix asynchronous tests
    The code the tests call are asynchronous, so the callback needs to tell
    the test when it’s finished.

--- test/spec/allowlist_spec.js
+++ test/spec/allowlist_spec.js
@@ -9,20 +9,22 @@ describe('allowlist', function () {
     (typeof allowlist.check).should.equal('function');
   });
 
-  it('should initialise with a default filename', function () {
+  it('should initialise with a default filename', function (ok) {
     allowlist.init(undefined, function() {
-        allowlist.check('foo.example.com').should.not.ok;
+        allowlist.check('foo.example.com').should.not.be.ok;
         allowlist.check('www.ukba.homeoffice.gov.uk').should.be.ok;
+        ok();
     });
   });
 
-  it('should initialise with a fixture filename', function () {
+  it('should initialise with a fixture filename', function (ok) {
     allowlist.init('test/fixtures/hosts.json', function() {
-        allowlist.check('www.ukba.homeoffice.gov.uk').should.not.ok;
+        allowlist.check('www.ukba.homeoffice.gov.uk').should.not.be.ok;
         allowlist.check('foo.example.com').should.be.ok;
         allowlist.check('bar.example.com').should.be.ok;
         allowlist.check('aka.example.com').should.not.be.ok;
         allowlist.check('aka-foo.example.com').should.not.be.ok;
+        ok();
     });
   });
 });
